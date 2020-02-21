bef04c316e2f2ce423384657fc2b3ee94704f8cc
aman-atg <amanansari.a2@gmail.com>
2020-02-21 23:01:36 +0530

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,33 @@
 
 const activeImg = document.querySelector(".active-work img"),
   images = document.querySelectorAll(".work-gallery img"),
-  activeDesc = document.querySelector(".active-img-desc");
+  activeDesc = document.querySelector(".active-img-desc"),
+  nav = document.querySelector("nav"),
+  menuBtn = document.querySelector(".menu-btn"),
+  closeBtn = document.querySelector(".close-btn"),
+  mobileNav = document.querySelector(".mobile-nav"),
+  mobileNavLinks = document.querySelectorAll(".mobile-nav a");
+
+// Mobile Nav
+
+menuBtn.addEventListener("click", () => {
+  mobileNav.classList.add("active");
+  closeBtn.style.display = "block";
+  menuBtn.style.display = "none";
+});
+closeBtn.addEventListener("click", () => {
+  mobileNav.classList.remove("active");
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "block";
+});
+// mob nav links
+mobileNavLinks.forEach(link =>
+  link.addEventListener("click", () => {
+    mobileNav.classList.remove("active");
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "block";
+  })
+);
 //   Array.prototype.map.images
 changeActiveImg = e => {
   activeImg.src = e.target.src;
@@ -10,3 +36,9 @@ changeActiveImg = e => {
 };
 images.forEach(img => img.addEventListener("click", changeActiveImg));
 // });
+
+// Scroll Menu add Class
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset >= 30) nav.classList.add("scrolled");
+  else nav.classList.remove("scrolled");
+});
